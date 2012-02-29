aa1eb0c36113cd50ace62e8d7526249e56c1bd0b
rudavis <rudavis@gmail.com>
2012-02-29 11:32:29 -0500

    removing another migration

--- db/migrate/20111216201023_remove_on_base_percentage_from_player.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOnBasePercentageFromPlayer < ActiveRecord::Migration
-  def up
-    remove_column :players, :on_base_percentage
-  end
-
-  def down
-    add_column :players, :on_base_percentage, :float
-  end
-end

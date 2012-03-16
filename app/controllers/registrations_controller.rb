80790e5799d5bf35f75ebd8d2e598227f7750932
rudavis <rudavis@gmail.com>
2012-03-16 09:52:15 -0400

    fixed change password flow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,10 @@ class RegistrationsController < Devise::RegistrationsController
 
 #This next section defines 1st user exp.
   def after_sign_up_path_for(resource)
-      edit_player_path(current_player)
+    edit_player_path(current_player)
+  end
+#After changing password
+  def after_update_path_for(resource)
+    player_path(current_player)
   end
 end

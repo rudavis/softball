e93c05bdb61888fbc0c756cb599987be791f479f
rudavis <rudavis@gmail.com>
2012-03-28 21:08:41 -0400

    fixing migration

--- db/migrate/20120328132520_add_admin_to_players.rb
+++ db/migrate/20120328132520_add_admin_to_players.rb
@@ -1,5 +1,5 @@
 class AddAdminToPlayers < ActiveRecord::Migration
   def change
-    add_column :players, :admin, :boolean, :default => 0
+    add_column :players, :admin, :boolean, :default => false
   end
 end
